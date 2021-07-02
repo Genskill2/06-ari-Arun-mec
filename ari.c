89642fdd2199aeaa1e0ab353193cb4235b02ad5b
Arun-mec <84711871+Arun-mec@users.noreply.github.com>
2021-07-02 11:55:53 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -18,9 +18,12 @@ const char* ari(char s[1000]){
             continue;
         }
     }
-
-    ari= (4.71 * (char_count/word_count)) + (0.5 * (word_count/sent_count)) - 21.43;
-
+    
+    printf("%d\n",char_count);
+    printf("%d\n",word_count);
+    printf("%d\n",sent_count);
+    ari= (4.71 * char_count/word_count) + (0.5 * word_count/sent_count) - 21.43;
+    printf("%d",ari);
     switch(ari){
         case 1 : return "Kindergarten";
         break;
