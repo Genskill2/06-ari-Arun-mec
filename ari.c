3e3bca3a2de8a4aa46fa1c95f59ca63a738706e1
Arun-mec <84711871+Arun-mec@users.noreply.github.com>
2021-07-02 12:28:02 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -25,36 +25,37 @@ const char* ari(char s[1000]){
     ari_calc=(4.71 * char_count/word_count) + (0.5 * word_count/sent_count) - 21.43;
     ari = (int)( ari_calc+0.5) / 1.;
     switch(ari){
-        case 1 : return "Kindergarten";
+        case 1 : str="Kindergarten";
         break;
-        case 2 : return "First/Second Grade";
+        case 2 : str="First/Second Grade";
         break;
-        case 3 : return "Third Grade";
+        case 3 : str= "Third Grade";
         break;
-        case 4 : return "Fourth Grade";
+        case 4 : str="Fourth Grade";
         break;
-        case 5 : return "Fifth Grade";
+        case 5 : str="Fifth Grade";
         break;
-        case 6 : return "Sixth Grade";
+        case 6 : str="Sixth Grade";
         break;
-        case 7 : return "Seventh Grade";
+        case 7 : str="Seventh Grade";
         break;
-        case 8 : return "Eighth Grade";
+        case 8 : str="Eighth Grade";
         break;
-        case 9 : return "Ninth Grade";
+        case 9 : str="Ninth Grade";
         break;
-        case 10 : return "Tenth Grade";
+        case 10 : str="Tenth Grade";
         break;
-        case 11 : return "Eleventh Grade";
+        case 11 : str="Eleventh Grade";
         break;
-        case 12 : return "Twelfth Grade";
+        case 12 : str="Twelfth Grade";
         break;
-        case 13 : return "College Student";
+        case 13 : str="College Student";
         break;
-        case 14 : return "Professor";
+        case 14 : str="Professor";
         break;
-        default: 
-            break;
+        default: break;
         }
+
+        return str;
    
 }
